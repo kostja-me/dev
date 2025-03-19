ca4e192c84d3c66381d32cdc02dc5bc1ef80dcab
Kostja <2241335+kostja-me@users.noreply.github.com>
2025-03-19 23:14:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -111,7 +111,9 @@ mise use --global ruby@3.3
 mise x ruby -- gem install rails --no-document
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node
 
 # desktop
