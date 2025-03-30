7e2b3eb4ca065c82186b491298eef8118ab2226b
Kostja <2241335+kostja-me@users.noreply.github.com>
2025-03-30 01:51:36 +0100

    Update install.sh
    some dependencies needed for jetbrains toolbox

--- install.sh
+++ install.sh
@@ -85,7 +85,8 @@ sudo apt install -y \
   build-essential pkg-config autoconf bison clang rustc \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
-  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
+  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common \
+  libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin tar dbus-user-session
 
 # mise
 # Install mise for managing multiple versions of languages. See https://mise.jdx.dev/
